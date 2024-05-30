89d2701b70262bfcf2015078492df70068f246a3
Agga <agga@Aggas-MacBook-Air.local>
2024-05-30 22:09:55 +0700

    [fix]

--- src/book/book_controller.py
+++ src/book/book_controller.py
@@ -21,20 +21,24 @@ class BookController:
         return await self.book_service.get_books(session)
 
     @Get('/:book_id')
-    async def get_book(self, book_id: int, session: AsyncSession = Depends(config.get_db)):
+    async def get_book(self,security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)],
+                       book_id: int, session: AsyncSession = Depends(config.get_db)):
         return await self.book_service.get_book(book_id, session)
 
     @Post("/")
-    async def add_book(self, book: Book, session: AsyncSession = Depends(config.get_db)):
+    async def add_book(self,security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)],
+                       book: Book, session: AsyncSession = Depends(config.get_db)):
         return await self.book_service.add_book(book, session)
 
     @Put('/:book_id')
-    async def update_book(self, book_id: int, book: Book, session: AsyncSession = Depends(config.get_db)):
+    async def update_book(self,security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)],
+                          book_id: int, book: Book, session: AsyncSession = Depends(config.get_db)):
         await self.book_service.update_book(book_id, book, session)
         return {"message": "Book updated successfully!", "book": book}
 
     #
     @Delete('/:book_id')
-    async def delete_book(self, book_id: int, session: AsyncSession = Depends(config.get_db)):
+    async def delete_book(self,security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)],
+                          book_id: int, session: AsyncSession = Depends(config.get_db)):
         result = await self.book_service.delete_book(book_id, session)
         return {"message": "Book deleted successfully!", "book": result}
